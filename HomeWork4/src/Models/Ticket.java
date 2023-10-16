b2a462eb2583a4ba35acda98e20633cbedd17b62
devana <devana728@rambler.ru>
2023-10-17 00:39:26 +0300

    homework4 added

--- HomeWork4/src/Models/Ticket.java
+++ HomeWork4/src/Models/Ticket.java
@@ -78,41 +78,17 @@ public class Ticket{
         return routeNumber;
     }
 
-
-    // public void setRouteNumber(int routeNumber) {
-    //     this.routeNumber = routeNumber;
-    // }
-
-
     public int getPlace() {
         return place;
     }
-
-
-    // public void setPlace(int place) {
-    //     this.place = place;
-    // }
-
 
     public int getPrice() {
         return price;
     }
 
-
-    // public void setPrice(int price) {
-    //     this.price = price;
-    // }
-
-
     public Date getDate() {
         return date;
     }
-
-
-    // public void setDate(Date date) {
-    //     this.date = date;
-    // }
-
 
     public boolean isValid() {
         return isValid;
@@ -123,43 +99,13 @@ public class Ticket{
         this.isValid = isValid;
     }
 
-
-    // public int getZonaStart() {
-    //     return zonaStart;
-    // }
-
-
     public void setZonaStart(int zonaStart) {
         this.zonaStart = zonaStart;
     }
-
-
-    // public int getZonaStop() {
-    //     return zonaStop;
-    // }
-
 
     public void setZonaStop(int zonaStop) {
         this.zonaStop = zonaStop;
     }
 
 
-    // public Date getTimeStart() {
-    //     return timeStart;
-    // }
-
-
-    // public void setTimeStart(Date timeStart) {
-    //     this.timeStart = timeStart;
-    // }
-
-
-    // public Date getTimeStop() {
-    //     return timeStop;
-    // }
-
-
-    // public void setTimeStop(Date timeStop) {
-    //     this.timeStop = timeStop;
-    // }
 }
